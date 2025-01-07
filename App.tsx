afaf4ff4138fde454be9807f81d5a2a2b3c197d5
Akshayba12 <akshaybajantri12@gmail.com>
2025-01-07 14:44:55 +0530

    created a navigation file

--- App.tsx
+++ App.tsx
@@ -7,37 +7,15 @@
 
 import React from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
 } from 'react-native';
+import Navigation from './src/navigation';
 
 
 function App(): React.JSX.Element {
 
   return (
-   <View style={styles.Container}>
-    <Text>Food app</Text>
-   </View>
+    <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  Container: {
-    flex:1,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
